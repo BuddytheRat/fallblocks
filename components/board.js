4d6cf5bc9af912195d24be3764448321fdcc27cf
buddytherat <buddytherat@gmail.com>
2016-06-27 20:52:03 -0500

    Tidy up code.

--- components/board.js
+++ components/board.js
@@ -1,8 +1,10 @@
 var Board = (function () {
 
-	var pub = {};
+	var exports = {};
+	
+	//Public//
 
-	pub.init = function() {
+	exports.init = function() {
 		border = new Phaser.BitmapData(game, 'border', BOARD_WIDTH, BOARD_HEIGHT);
 		border.fill(180, 180, 230);
 		game.add.sprite(BOARD_OFFSET_X, BOARD_OFFSET_Y, border);
@@ -10,25 +12,29 @@ var Board = (function () {
 		draw_grid();
 	}
 
-
-	//Public//
-
-	pub.occupied = function(x, y) {
+	exports.occupied = function(x, y) {
 		//return true if tile at [x, y] is occupied
 	};
-	pub.unoccupied = function(x, y) {
+	exports.unoccupied = function(x, y) {
 		return !this.occupied(x, y);
 	};
-	pub.clear_line = function(y) {
+	exports.clear_line = function(y) {
 		//magic
 	};
 
 	//Private//
+
+	var new_matrix = function () {
+		this.board_matrix = new Array(this.BOARD_HEIGHT);
+		for (var y = 0; y < this.BOARD_HEIGHT; y++) {
+			this.board_matrix[y] = new Array(this.BOARD_WIDTH);
+		}
+	};
+
 	var draw_grid = function () {
 		var color = "#99b";
 
 		for (var i = 1; i <= BOARD_TILE_WIDTH - 1; i++) {
-			console.log(i);
 			var line = new Phaser.Line(
 				BOARD_OFFSET_X + (TILE_WIDTH * i),
 				BOARD_OFFSET_Y,
@@ -38,7 +44,6 @@ var Board = (function () {
 			game.debug.geom(line, color);
 		}
 		for (var i = 1; i <= BOARD_TILE_HEIGHT - 1; i++) {
-			console.log(i);
 			var line = new Phaser.Line(
 				BOARD_OFFSET_X,
 				BOARD_OFFSET_Y + (TILE_HEIGHT * i),
@@ -49,13 +54,7 @@ var Board = (function () {
 		}
 
 	};
-	var new_matrix = function () {
-		this.board_matrix = new Array(this.BOARD_HEIGHT);
-		for (var y = 0; y < this.BOARD_HEIGHT; y++) {
-			this.board_matrix[y] = new Array(this.BOARD_WIDTH);
-		}
-	};
 
-	return pub;
+	return exports;
 
 }());
